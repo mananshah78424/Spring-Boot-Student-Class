c7de06d24f3abc08fe300f52f2877d7b8e497d04
mananshah78424 <manan78424@gmail.com>
2024-08-28 21:46:14 -0700

    Initial commit

---
+++ src/main/java/com/learning2/demo/student/Student.java
@@ -0,0 +1,9 @@
+package com.learning2.demo.student;
+
+public class Student {
+    private long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+}
